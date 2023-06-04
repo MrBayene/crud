7e2b73dfcf7883eade9eba2b76971860c7e3f74e
Ivan Rodriguez <ivan.rodriguez10@ingka.ikea.com>
2023-06-04 21:03:28 +0200

    set up first test and testDB. test-helper is old

---
+++ test/controller/category.test.ts
@@ -0,0 +1,47 @@
+import { TransactionType } from "./../../src/controller/Transaction";
+import { setCategory } from "../../src/controller/Category";
+import { assert } from "chai";
+import * as helper from "../utility/test-helper";
+import * as db from "../utility/testDB";
+import * as mongoose from "mongoose";
+
+
+describe("Testing the Category Controller", () => {
+  const transNoCat: TransactionType = {
+    TransactionDate: "11-11-1111",
+    RegisterDate: "11-11-1111",
+    Name: "NoCatTrans",
+    Description: "A transaction without a category",
+    Amount: "111",
+    Balance: "222",
+    Category:"",
+  };
+  const transWithCat: TransactionType = {
+    TransactionDate: "22-22-2222",
+    RegisterDate: "22-22-2222",
+    Name: "CatTrans",
+    Description: "A transaction with a category",
+    Amount: "222",
+    Balance: "111",
+    Category: "TestCategory",
+  };
+
+
+  before(async () => {
+    await db.connect();
+  });
+  after(async () => {
+    await db.closeDatabase();
+  });
+
+  it("Category already exist in both  the Transaction and Category documents -> Do nothing", async () => {
+    await db.addCategory(transWithCat.Name,transWithCat.Description,transWithCat.Category);
+    const before:number=await db.countCats();
+    await setCategory(transWithCat.Name,transWithCat.Description,transWithCat.Category);
+    const after:number=await db.countCats();
+    assert.equal(before,after)
+  });
+
+});
+
+
